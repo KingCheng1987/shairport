051a71aca56c77e0d0ebe38f586cd53d299a19ba
³̽ðÁú¡ <hzchengjinlong@corp.netease.com>
2017-11-14 11:27:05 +0800

    fix

--- mdns.c
+++ mdns.c
@@ -95,7 +95,7 @@ void mdns_register(void) {
             if (error >= 0)
             {
                 config.mdns = *b;
-				n("The correct mdns is %s\r\n",(*b)->name);
+				("The correct mdns is %s\r\n",(*b)->name);
                 break;
             }
         }
